652c5f27f2914a2e4955eb2d4c1bb54cd1ed33ba
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 13:47:58 -0300

    feat: refactor ExamRecord model to use DateTime for exam date and time, and add JSON serialization methods

--- lib/models/exam_model.dart
+++ lib/models/exam_model.dart
@@ -1,13 +1,27 @@
 class ExamRecord {
   final String examType;
   final String examLocation;
-  final String examDate;
-  final String examTime;
+  final DateTime examDateTime;
 
   ExamRecord({
     required this.examType,
     required this.examLocation,
-    required this.examDate,
-    required this.examTime,
+    required this.examDateTime,
   });
+
+  factory ExamRecord.fromJson(Map<String, dynamic> json) {
+    return ExamRecord(
+      examType: json['examType'] ?? json['exam_type'] ?? '',
+      examLocation: json['examLocation'] ?? json['exam_location'] ?? '',
+      examDateTime: DateTime.tryParse((json['examDate'] ?? json['exam_date']).toString()) ?? DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'examType': examType,
+      'examLocation': examLocation,
+      'examDate': examDateTime.toIso8601String(),
+    };
+  }
 }
