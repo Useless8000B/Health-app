9b73e9ba522bce8e9ca6ef868fb0dbbb2e9ed402
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 10:46:26 -0300

    Split examDate into date and time

--- lib/models/exam_model.dart
+++ lib/models/exam_model.dart
@@ -2,10 +2,12 @@ class ExamRecord {
   final String examType;
   final String examLocation;
   final String examDate;
+  final String examTime;
 
   ExamRecord({
     required this.examType,
     required this.examLocation,
     required this.examDate,
+    required this.examTime,
   });
 }
