0352bbbefebf0357244473dc5f6e549e35a20f5b
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 13:47:40 -0300

    feat: integrate Firebase initialization and implement user authentication flow with loading state handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:health/firebase_options.dart';
 import 'package:health/colors.dart';
+import 'package:health/screens/login_screen.dart';
 import 'package:health/screens/shell_screen.dart';
+import 'package:health/services/auth_services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Health());
 }
 
@@ -17,7 +23,7 @@ class Health extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: AppColors.primary,
-          brightness: Brightness.dark
+          brightness: Brightness.dark,
         ),
         brightness: Brightness.dark,
         scaffoldBackgroundColor: AppColors.background,
@@ -26,11 +32,26 @@ class Health extends StatelessWidget {
           titleTextStyle: TextStyle(
             color: AppColors.white,
             fontSize: 24,
-            fontWeight: FontWeight.w500
-          )
-        )
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
-      home: const ShellScreen(),
+      home: StreamBuilder(
+        stream: AuthService().user,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const ShellScreen();
+          }
+          
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
