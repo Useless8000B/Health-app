1af98949f249bb052e9ef7be4edbccda6d71d83b
Useless8000B <adrielluiz73@gmail.com>
2026-04-23 22:59:03 -0300

    feat: implement UI for settings screen and apply custom dark theme to app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,22 @@ class Health extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Health',
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark
+        ),
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Color(0xFF181A20),
+        appBarTheme: AppBarThemeData(
+          backgroundColor: Color(0xFF181A20),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w500
+          )
+        )
+      ),
       home: const ShellScreen(),
     );
   }
