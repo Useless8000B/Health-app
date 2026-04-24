d2272537e2070ca6a17adcc259fa6c33e1fa4544
Useless8000B <adrielluiz73@gmail.com>
2026-04-24 10:33:14 -0300

    feat: add ExamRecord model and implement schedule exam UI, while refactoring theme colors to AppColors constant

---
+++ lib/models/exam_model.dart
@@ -0,0 +1,11 @@
+class ExamRecord {
+  final String examType;
+  final String examLocation;
+  final String examDate;
+
+  ExamRecord({
+    required this.examType,
+    required this.examLocation,
+    required this.examDate,
+  });
+}
